da00c80a433aadd34fa3877a2e8dbcb13e4ef1fd
ttmuangkeo <tim.muangkeo@gmail.com>
2017-06-13 13:57:36 -0700

    added scope itunes

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -1,30 +1,17 @@
 angular.module('myCtrls', ['somethingServices'])
     .controller('HomeCtrl', ['$scope', '$http', function($scope, $http) {
-        var artist = 'drake'
-        $http({
-        url: 'http://api.musixmatch.com/ws/1.1/artist.search',
-        params: {
-        callback: 'callback',
-        format: 'json',
-            'apikey': 'fdc9ad9d9fa9d41f48a88a72c770f024',
-            'f_artist_id': '',
-            'q_artist': artist 
+        
+        $scope.search = function(){
+            $http({
+                url: 'https://itunes.apple.com/search?&term=?' + $scope.artist,
+                params: {
+                    limit: 5
+                }
+            }).then(function succes(req) {
+                console.log('what is this', req)
+                $scope.test = req.data.results
+            }).catch(function error(err) {
+                console.log('what is the erro', err)
+            });
         }
-    }).then(function succes(req) {
-        console.log('what is this', req.data.message.body)
-        $scope.test = req.data.message.body.artist_list
-    }).catch(function error(err) {
-        console.log('what is the erro', err)
-    });
 }])
-    .controller('ArtistCtrl', ['$scope', '$http', function($scope, $http) {
-        $http({
-            url: 'http://api.musixmatch.com/ws/1.1/artist.get',
-            params: {
-                callback: 'callback',
-                format: 'json',
-                'apikey': 'fdc9ad9d9fa9d41f48a88a72c770f024',
-                artist_id: ''
-            }
-        })
-    }])
