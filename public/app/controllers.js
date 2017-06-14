3d6e5e61595ad4b6d3e136f27a88fb7eb2e53145
ttmuangkeo <tim.muangkeo@gmail.com>
2017-06-14 13:53:54 -0700

    styled lyrics page

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -1,62 +1,89 @@
 angular.module('myCtrls', ['somethingServices'])
-    .controller('HomeCtrl', ['$scope', '$http', function($scope, $http) {
-        
-        $scope.search = function(){
+    .controller('HomeCtrl', ['$scope', '$http', '$location', function($scope, $http, $location) {
+        $scope.artist;
+
+        $scope.search = function() {
             $http({
-                url: 'https://itunes.apple.com/search?&term=?' + $scope.artist,
-                params: {
-                    limit: 20
-                }
+                url: 'https://itunes.apple.com/search?term=' + $scope.artist,
+                    params: {
+                    'entity': 'album',
+                    'limit': 6,
+
+                    }
             }).then(function succes(req) {
                 console.log('what is this', req)
-                $scope.test = req.data.results
+                $scope.test = req.data.results;
             }).catch(function error(err) {
                 console.log('what is the erro', err)
             });
-        }
-}])
-    .controller('musicCtrl', ['$scope', '$http', function($scope, $http) {
+}
+    }])
+
+
+
+
+
+
+
+.controller('musicCtrl', ['$scope', '$http', function($scope, $http) {
         $scope.search = function() {
             $http({
                 url: 'https://itunes.apple.com/search?term=' + $scope.artist + '&entity=musicVideo'
-            }).then(function success (req) {
+            }).then(function success(req) {
                 console.log('is this the music video?', req);
                 $scope.test = req.data.results
-            }).catch(function error (err) {
+            }).catch(function error(err) {
                 console.log('give me an error baby..', err)
             });
         };
     }])
     .controller('lyricsCtrl', ['$scope', '$http', function($scope, $http) {
         $scope.search = function() {
-            
+
             $http({
-                url: 'https://api.musixmatch.com/ws/1.1/matcher.lyrics.get', 
-                    'params': {
-                        'format': 'json',
-                        'callback': 'callback',
-                        'q_track': $scope.artist,
-                        'q_artist': $scope.title,
-                        'apikey': 'fdc9ad9d9fa9d41f48a88a72c770f024',
-                    }
-            }).then(function success (req) {
+                url: 'https://api.musixmatch.com/ws/1.1/matcher.lyrics.get',
+                'params': {
+                    'format': 'json',
+                    'callback': 'callback',
+                    'q_track': $scope.artist,
+                    'q_artist': $scope.title,
+                    'apikey': 'fdc9ad9d9fa9d41f48a88a72c770f024',
+                }
+            }).then(function success(req) {
                 console.log('give me my data', req.data.message.body.lyrics);
                 $scope.test = req.data.message.body.lyrics;
-            }).catch(function error (err) {
+            }).catch(function error(err) {
                 console.log('error bro.', err);
             });
+
             $http({
-                url: 'https://itunes.apple.com/search?&term=?' + $scope.artist,
-                params: {
-                    limit: 20
-                }
+                url: 'https://itunes.apple.com/search?term=' + $scope.artist
             }).then(function succes(req) {
                 console.log('what is this', req)
-                $scope.test = req.data.results
+                $scope.songInfo = req.data.results[0];
             }).catch(function error(err) {
                 console.log('what is the erro', err)
             });
-
-        }
+        };
 
     }])
+
+
+.controller('songCtrl', ['$scope', '$http', function($scope, $http) {
+    $scope.search = function() {
+        $http({
+            url: 'https://api.musixmatch.com/ws/1.1/matcher.track.get',
+            'params': {
+                'format': 'json',
+                'callback': 'callback',
+                'q_track': $scope.artist,
+                'q_artist': $scope.title,
+                'apikey': 'fdc9ad9d9fa9d41f48a88a72c770f024',
+            }
+        }).then(function success(req) {
+            console.log('musixmatch bro', req);
+        }).catch(function error(err) {
+            console.log('error:', err)
+        })
+    };
+}]);
